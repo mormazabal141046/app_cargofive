5e72b1f28632e4ee8fc4a65aec6b50b43a22eb95
mormazabal <mormazabal@bcnschool.cl>
2022-03-08 05:15:51 -0300

    Cargo Five Deploy V2

---
+++ dist/precache-manifest.cf37e3b1ea8a7613d175e36a140e28e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e91611a0fef825e99389",
+    "url": "/css/chunk-19a776a2.a4c059ea.css"
+  },
+  {
+    "revision": "c4924f22e3a48b8ae1fb",
+    "url": "/css/chunk-vendors.b5c8f5ea.css"
+  },
+  {
+    "revision": "5f3ddd7d8d15559fbf405816051211ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f076e3fbfd81176232d",
+    "url": "/js/app.0555bbc5.js"
+  },
+  {
+    "revision": "e91611a0fef825e99389",
+    "url": "/js/chunk-19a776a2.50a6a9bf.js"
+  },
+  {
+    "revision": "c4924f22e3a48b8ae1fb",
+    "url": "/js/chunk-vendors.ede9f810.js"
+  },
+  {
+    "revision": "b7986635186d535c6346af442e944ad0",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
